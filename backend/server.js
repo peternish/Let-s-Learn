58a0e2f0c88b2598b69c1ea3553f87b4fe383108
peternish <peter95613@gmail.com>
2020-05-13 13:17:06 +0530

    shivesh

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 var express    =  require("express");
 var login =  require('./routes/loginroutes');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 require('./dbconnection');
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
@@ -10,15 +12,24 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
 var router = express.Router();
+
+app.get('/', (req, res) => res.send('ho!'))
+
 // test route
-router.get('/', function(req, res) {
-    res.json({ message: 'welcome to our upload module apis' });
-});
+// router.get('/', function(req, res) {
+//     res.json({ message: 'welcome to our upload module apis' });
+// });
 //route to handle user registration
-router.post('/register',login.register);
-router.post('/login',login.login)
-app.use('/api', router);
+// router.post('/register',login.register);
+
+// router.get('/register',(res)=>{
+//     console.log("hello");
+// });
+
+// router.post('/login',login.login)
+// app.use('/api', router);
 
 app.listen(8081,()=>{ 
     console.log("Server is Listening At Port 8081")  
