ec9f94042d18cba2172613a11b613cceab1e1a7d
peternish <peter95613@gmail.com>
2020-05-29 14:51:27 +0530

    Shivani

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -233,17 +233,17 @@ deleten = id => {
             <div id="wrapper">
     <ul className="navbar-nav  sidebar sidebar-dark accordion" id="accordionSidebar">
 
-      <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/studentDashboard">
         <div className="sidebar-brand-icon rotate-n-15">
           <i className="fas fa-laugh-wink"></i>
         </div>
         <div className="sidebar-brand-text mx-3">Lets's Learn</div>
-      </a>
+      </Link>
       <hr className="sidebar-divider my-0"/>
       <li className="nav-item active">
-        <a className="nav-link" href="index.html">
+        <Link className="nav-link" to="/studentDashboard">
           <i className="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
+          <span>Dashboard</span></Link>
       </li>
       <hr className="sidebar-divider"/>
       <li className="nav-item">
